91fe699311bbf77125b0252c884afddca218893b
ginowine <emailosahon@yahoo.com>
2019-08-02 09:10:40 +0100

    menu button created

--- app/src/main/java/com/example/travelmantics/InsertActivity.java
+++ app/src/main/java/com/example/travelmantics/InsertActivity.java
@@ -2,6 +2,8 @@ package com.example.travelmantics;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
 
 public class InsertActivity extends AppCompatActivity {
 
@@ -9,5 +11,12 @@ public class InsertActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_insert);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.save_menu, menu);
+        return true;
     }
 }
